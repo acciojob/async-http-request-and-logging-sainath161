bb58a61a1a7dd96e3b2243f226dd39b356a28f45
Sainath <71361447+sainath161@users.noreply.github.com>
2024-03-25 13:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+async function fetchTodo() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const todo = await response.json();
+    console.log('Todo:', todo);
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
+}
+
+fetchTodo();
